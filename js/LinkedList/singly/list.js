25054c85f37da5539b78e76bdbac3cb5822eea6d
harris-ahmad <24100315@lums.edu.pk>
2024-07-15 01:55:17 +0500

    insertion and deletion functions implemented

--- js/LinkedList/singly/list.js
+++ js/LinkedList/singly/list.js
@@ -21,4 +21,58 @@ class LinkedList {
     }
     return lastNode;
   }
+
+  prepend(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      const lastNode = this.getTail();
+      lastNode.next = newNode;
+    }
+  }
+
+  insertBetween(data, after) {
+    const newNode = new Node(data);
+    let prevNode = this.head;
+
+    while (prevNode && prevNode.data !== after) {
+      prevNode = prevNode.next;
+    }
+
+    if (prevNode) {
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+    }
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return new Error("list is empty, nothing to delete");
+    }
+    if (this.head) {
+      this.head = this.head = next;
+    }
+  }
+
+  deleteTail() {
+    if (!this.head) {
+      return new Error("list is empty, nothing to delete");
+    }
+    if (!this.head.next) {
+      this.head = null;
+    } else {
+      let prevNode = this.head;
+      while (prevNode.next && prevNode.next.next) {
+        prevNode = prevNode.next;
+      }
+      prevNode.next = null;
+    }
+  }
 };
