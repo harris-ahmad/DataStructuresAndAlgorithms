a16262b736ccf0027b26f7802c172edfc6483813
harris-ahmad <24100315@lums.edu.pk>
2024-07-15 02:42:00 +0500

    mutex added for thread safety

--- go/LinkedList/singly/list.go
+++ go/LinkedList/singly/list.go
@@ -1,30 +1,44 @@
-package singly
+package main
 
+import (
+	"sync"
+)
+
+// Node represents a single node in a singly linked list.
 type Node struct {
 	data interface{}
 	next *Node
 }
 
+// LinkedList represents a singly linked list.
 type LinkedList struct {
 	head    *Node
 	tail    *Node
 	length  int
-	nodeMap map[int]*Node
+	nodeMap map[interface{}]*Node // Map for O(1) lookups
+	mutex   sync.RWMutex          // Mutex for thread-safe operations
 }
 
+// NewLinkedList creates an empty LinkedList.
 func NewLinkedList() *LinkedList {
 	return &LinkedList{
 		head:    nil,
 		tail:    nil,
 		length:  0,
-		nodeMap: make(map[int]*Node),
+		nodeMap: make(map[interface{}]*Node),
 	}
 }
 
+// GetHead returns the head node of the list.
 func (ll *LinkedList) GetHead() *Node {
+	ll.mutex.RLock()
+	defer ll.mutex.RUnlock()
 	return ll.head
 }
 
+// GetTail returns the tail node of the list.
 func (ll *LinkedList) GetTail() *Node {
+	ll.mutex.RLock()
+	defer ll.mutex.RUnlock()
 	return ll.tail
 }
