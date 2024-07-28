94260b6c44c8d02fb311ccea05115a87bdb34600
harris-ahmad <24100315@lums.edu.pk>
2024-07-28 16:31:26 +0500

    deletion functions added

--- c/LinkedList/singly/list.c
+++ c/LinkedList/singly/list.c
@@ -2,85 +2,138 @@
 #include <stdlib.h>
 
 typedef struct Node {
-	void *data;
-	struct Node *next;
-	int ref_count;
+  void *data;
+  struct Node *next;
+  int ref_count;
 } Node;
 
 typedef struct LinkedList {
-	Node *head;
+  Node *head;
 } LinkedList;
 
 // functions for garbage collection
-void incrementReference(Node *node) {
-	if (node) {
-		node->ref_count++;
-	}
+void incrementRefCount(Node *node) {
+  if (node) {
+    node->ref_count++;
+  }
 }
 
-void decrementReference(Node *node) {
-	if (node) {
-		node->ref_count--;
-		if (node->ref_count <= 0) {
-			free(node);
-		}
-	}
+void decrementRefCount(Node *node) {
+  if (node) {
+    node->ref_count--;
+    if (node->ref_count <= 0) {
+      free(node);
+    }
+  }
 }
 
 Node *createNode(void *data) {
-	Node *newNode = (Node *)malloc(sizeof(Node));
-	newNode->data = data;
-	newNode->next = NULL;
-	newNode->ref_count = 1;
-	return newNode;
+  Node *newNode = (Node *)malloc(sizeof(Node));
+  newNode->data = data;
+  newNode->next = NULL;
+  newNode->ref_count = 1;
+  return newNode;
 }
 
 LinkedList *createLinkedList() {
-	LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
-	list->head = NULL;
-	return list;
+  LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+  list->head = NULL;
+  return list;
 }
 
 Node *getHead(LinkedList *list) { return list->head; }
 
 Node *getTail(LinkedList *list) {
-	if (!list->head)
-		return (Node *)NULL;
-	else {
-		Node *lastNode = list->head;
-		while (lastNode && lastNode->next) {
-			lastNode = lastNode->next;
-		}
-		return lastNode;
-	}
+  if (!list->head)
+    return (Node *)NULL;
+  else {
+    Node *lastNode = list->head;
+    while (lastNode && lastNode->next) {
+      lastNode = lastNode->next;
+    }
+    return lastNode;
+  }
 }
 
 void prepend(LinkedList *list, void *data) {
-	Node *newNode = createNode(data);
-	newNode->next = list->head;
-	list->head = newNode;
+  Node *newNode = createNode(data);
+  newNode->next = list->head;
+  list->head = newNode;
 }
 
 void append(LinkedList *list, void *data) {
-	Node *newNode = createNode(data);
-	if (!list->head) {
-		list->head = newNode;
-	} else {
-		Node *lastNode = getTail(list);
-		lastNode->next = newNode;
-	}
+  Node *newNode = createNode(data);
+  if (!list->head) {
+    list->head = newNode;
+  } else {
+    Node *lastNode = getTail(list);
+    lastNode->next = newNode;
+  }
 }
 
 void insertBetween(LinkedList *list, void *data, void *after) {
-    Node *newNode = createNode(data);
+  Node *newNode = createNode(data);
+  Node *prevNode = list->head;
+
+  while (prevNode && prevNode->data != after) {
+    prevNode = prevNode->next;
+  }
+
+  if (prevNode) {
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+  }
+}
+
+// Function to delete the head node
+void deleteHead(LinkedList *list) {
+  if (list->head) {
+    Node *temp = list->head;
+    list->head = list->head->next;
+    decrementRefCount(temp);
+  }
+}
+
+// Function to delete the tail node
+void deleteTail(LinkedList *list) {
+  if (!list->head) {
+    return;
+  }
+  if (!list->head->next) {
+    decrementRefCount(list->head);
+    list->head = NULL;
+  } else {
     Node *prevNode = list->head;
+    while (prevNode->next && prevNode->next->next) {
+      prevNode = prevNode->next;
+    }
+    decrementRefCount(prevNode->next);
+    prevNode->next = NULL;
+  }
+}
 
-    while (prevNode && prevNode->data != after) {
-        prevNode = prevNode->next;
+// Function to delete any node with a specific value
+void deleteAny(LinkedList *list, void *target) {
+  if (!list->head) {
+    return;
+  }
+
+  if (list->head->data == target) {
+    Node *temp = list->head;
+    list->head = list->head->next;
+    decrementRefCount(temp);
+  } else {
+    Node *prevNode = list->head;
+    Node *curr = list->head->next;
+
+    while (curr && curr->data != target) {
+      prevNode = curr;
+      curr = curr->next;
     }
 
-    if (prevNode) {
-        newNode->next = prevNode->next;
-        prevNode->next = newNode;
+    if (curr) {
+      prevNode->next = curr->next;
+      decrementRefCount(curr);
     }
+  }
 }
