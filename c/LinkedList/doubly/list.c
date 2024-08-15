b8a2116071f3fe282fef2cc72b557f9472405aac
harris-ahmad <24100315@lums.edu.pk>
2024-08-15 05:41:44 +0500

    base code set

---
+++ c/LinkedList/doubly/list.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+typedef struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} Node;
+
+// Doubly Linked List structure
+typedef struct DoublyLinkedList {
+    Node* head;
+    Node* tail;
+} DoublyLinkedList;
+
+// Function to create a new node
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+// Function to initialize an empty list
+DoublyLinkedList* createList() {
+    DoublyLinkedList* list = (DoublyLinkedList*)malloc(sizeof(DoublyLinkedList));
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
