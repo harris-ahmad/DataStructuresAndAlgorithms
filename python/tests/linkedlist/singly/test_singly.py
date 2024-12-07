04145e4b3f1bafb2c3115d5241e2b3fbcb0b6680
Harris Ahmad <harris.ahmad@linq.io>
2024-12-08 00:10:44 +0500

    code prettified

--- python/tests/linkedlist/singly/test_singly.py
+++ python/tests/linkedlist/singly/test_singly.py
@@ -4,6 +4,7 @@ import pytest
 
 NUM_ELEMENTS = 100_000
 
+
 @pytest.fixture
 def linked_list_append_fixture() -> LinkedList:
     ll = LinkedList()
@@ -12,6 +13,7 @@ def linked_list_append_fixture() -> LinkedList:
         ll.append(f"node({i})")
         
     return ll
+
 
 @pytest.fixture
 def linked_list_prepend_fixture() -> LinkedList:
@@ -22,9 +24,18 @@ def linked_list_prepend_fixture() -> LinkedList:
     
     return ll
 
+
 def test_append(linked_list_append_fixture):
     ll = linked_list_append_fixture
     
     assert len(ll) == NUM_ELEMENTS
     assert ll.get_tail().data == f"node({NUM_ELEMENTS-1})"
     assert ll.get_head().data == "node(0)"
+    
+
+def test_prepend(linked_list_prepend_fixture):
+    ll = linked_list_prepend_fixture
+    
+    assert len(ll) == NUM_ELEMENTS
+    assert ll.get_tail().data == f"node({NUM_ELEMENTS-1})"
+    assert ll.get_head().data == f"node({NUM_ELEMENTS-99999})"
