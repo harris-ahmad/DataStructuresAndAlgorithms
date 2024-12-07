8976768e7a83fd1dd59ad85f775ad7ccd2b5268d
Harris Ahmad <harris.ahmad@linq.io>
2024-12-07 21:46:24 +0500

    singly list impl improved

--- python/LinkedList/singly/list.py
+++ python/LinkedList/singly/list.py
@@ -1,54 +1,34 @@
-class Node:
-    """
-    Node class representing a single node in a singly linked list.
-    """
+import os
+from typing import List, Dict
 
+class Node: 
     def __init__(self, data):
-        """
-        Create a Node.
-
-        :param data: The data to be stored in the node.
-        """
         self.data = data
         self.next = None
-
+        self._id = os.urandom(16).hex()
+        
+    def __repr__(self) -> str:
+        return f"Node({self._id}: {self.data})"
 
 class LinkedList:
-    """
-    LinkedList class representing a singly linked list.
-    """
-
     def __init__(self):
-        """
-        Create an empty LinkedList.
-        """
         self.head = None
         self.tail = None
         self.length = 0
-        self.node_map = {}  # Dictionary for O(1) lookups
+        # dictionary to store nodes by their IDs for faster lookup and deletion
+        self.node_map = {}
 
     def get_head(self):
-        """
-        Get the head node of the list.
-
-        :return: The head node or None if the list is empty.
-        """
+        """Get the head node of the list."""
         return self.head
 
     def get_tail(self):
-        """
-        Get the tail node of the list.
-
-        :return: The tail node or None if the list is empty.
-        """
+        """Get the tail node of the list."""
         return self.tail
 
     def prepend(self, data):
-        """
-        Prepend a node to the beginning of the list.
-
-        :param data: The data to be stored in the new node.
-        """
+        """Prepend a node to the start of the list."""
+        
         new_node = Node(data)
         if not self.head:
             self.head = new_node
@@ -56,15 +36,12 @@ class LinkedList:
         else:
             new_node.next = self.head
             self.head = new_node
-        self.node_map[data.id] = new_node
+        self.node_map[new_node._id] = new_node.data
         self.length += 1
 
     def append(self, data):
-        """
-        Append a node to the end of the list.
-
-        :param data: The data to be stored in the new node.
-        """
+        """Append a node to the end of the list."""
+        
         new_node = Node(data)
         if not self.head:
             self.head = new_node
@@ -72,7 +49,7 @@ class LinkedList:
         else:
             self.tail.next = new_node
             self.tail = new_node
-        self.node_map[data.id] = new_node
+        self.node_map[new_node._id] = new_node.data
         self.length += 1
 
     def insert_between(self, data, before, after):
@@ -84,6 +61,9 @@ class LinkedList:
                        insert the new node.
         :param after: The data of the node after which to insert the new node.
         """
+        if not self.head:
+            raise Exception("List is empty, nothing to insert")
+        
         new_node = Node(data)
         curr = self.head
 
@@ -93,50 +73,49 @@ class LinkedList:
         if curr and curr.next and curr.next.data == before:
             new_node.next = curr.next
             curr.next = new_node
-            self.node_map[data.id] = new_node
+            self.node_map[new_node._id] = new_node.data
             self.length += 1
 
     def delete_head(self):
-        """
-        Delete the head node of the list.
-        """
+        """Delete the head node of the list."""
+        
         if self.head:
             self.node_map.pop(self.head.data.id, None)
             self.head = self.head.next
             if not self.head:
                 self.tail = None
             self.length -= 1
+        else:
+            raise Exception("List is empty, nothing to delete")
 
     def delete_tail(self):
         """
         Delete the tail node of the list.
         """
         if not self.head:
-            return
+            raise Exception("List is empty, nothing to delete")
+        
         if not self.head.next:
-            self.node_map.pop(self.head.data.id, None)
+            self.node_map.pop(self.head._id, None)
             self.head = None
             self.tail = None
         else:
             prev_node = self.head
             while prev_node.next and prev_node.next.next:
                 prev_node = prev_node.next
-            self.node_map.pop(prev_node.next.data.id, None)
+            self.node_map.pop(prev_node.next._id, None)
             prev_node.next = None
             self.tail = prev_node
         self.length -= 1
 
     def delete_any(self, target):
-        """
-        Delete any node from the list that matches the given target data.
-
-        :param target: The target data to match for deletion.
-        """
+        """Delete any node from the list that matches the given target data."""
+        
         if not self.head:
-            return
+            raise Exception("List is empty, nothing to delete")
 
-        if self.head.data.id == target.id:
-            self.node_map.pop(self.head.data.id, None)
+        if self.head._id == target._id:
+            self.node_map.pop(self.head._id, None)
             self.head = self.head.next
             if not self.head:
                 self.tail = None
@@ -153,21 +132,19 @@ class LinkedList:
                 prev_node.next = curr.next
                 if not curr.next:
                     self.tail = prev_node
-                self.node_map.pop(curr.data.id, None)
+                self.node_map.pop(curr._id, None)
                 self.length -= 1
 
     def size(self):
-        """
-        Get the size of the list.
-
-        :return: The number of nodes in the list.
-        """
+        """Get the number of nodes in the list."""
+        
         return self.length
 
     def reverse_list(self):
         """
         Reverse the linked list in place.
         """
+        
         prev = None
         curr = self.head
         next_node = None
@@ -179,16 +156,17 @@ class LinkedList:
             curr.next = prev
             prev = curr
             curr = next_node
+        
         self.head = prev
 
-    def find(self, id):
+    def find(self, node: Node):
         """
         Find a node in the list by its id.
 
         :param id: The id of the node to find.
         :return: The node with the given id or None if not found.
         """
-        return self.node_map.get(id, None)
+        return self.node_map.get(node._id, None)
 
     def clear(self):
         """
@@ -209,3 +187,4 @@ class LinkedList:
             result.append(str(curr.data))
             curr = curr.next
         print(" -> ".join(result))
+        
